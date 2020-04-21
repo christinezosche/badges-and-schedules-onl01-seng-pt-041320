cd43f5daf83342fba4bcaae94b856c38276a30ba
Christine Zosche <christine.zosche@gmail.com>
2020-04-21 22:12:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,8 @@
-# Write your code here.
+def batch_badge_creator(names)
+  badge_messages = []
+  names.each do |name|
+    badge_messages << "Hello, my name is #{name}."
+  end
+badge_messages
+end
+
