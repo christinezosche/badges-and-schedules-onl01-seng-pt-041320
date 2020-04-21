ccff642d1f312967ffc7bb4494717e44923e0a8f
Christine Zosche <christine.zosche@gmail.com>
2020-04-21 22:29:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 def assign_rooms(names)
   room_assignments = []
   names.each do |name|
-    room_assignments << "Hello, #{name}! You'll be assigned to room #{room}!"
+    room_assignments << "Hello, #{name}! You'll be assigned to room #{name.index + 1}!"
   end
   end
 room_assignments
